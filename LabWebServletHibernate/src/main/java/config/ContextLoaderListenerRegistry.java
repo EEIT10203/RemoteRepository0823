ee76066cc886b583c7681cf38baf13913bf72eba
劉芷妤 <10241@EEIT>
2018-08-23 12:27:24 +0800

    沒註解

--- LabWebServletHibernate/src/main/java/config/ContextLoaderListenerRegistry.java
+++ LabWebServletHibernate/src/main/java/config/ContextLoaderListenerRegistry.java
@@ -12,7 +12,7 @@ public class ContextLoaderListenerRegistry extends AbstractContextLoaderInitiali
 		context.register(SpringJavaConfiguration.class);
 		return context;
 		
-		//jshklzjfkgjnzng;kl
+
 		
 	}
 }
