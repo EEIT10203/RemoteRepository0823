d129a0df80ceae1b56ceb56776e709cae42ecd6e
沈詩涵 <10209@EEIT>
2018-08-23 12:28:35 +0800

    11111

--- LabWebServletHibernate/src/main/java/config/ContextLoaderListenerRegistry.java
+++ LabWebServletHibernate/src/main/java/config/ContextLoaderListenerRegistry.java
@@ -11,5 +11,5 @@ public class ContextLoaderListenerRegistry extends AbstractContextLoaderInitiali
 				new AnnotationConfigWebApplicationContext();
 		context.register(SpringJavaConfiguration.class);
 		return context;
-	}
+	}//11122
 }
